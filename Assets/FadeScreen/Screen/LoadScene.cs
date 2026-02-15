1d100b1ed76819292054d615b349482c5cdcb72d
JosefinoXP <joselucasxp345@gmail.com>
2026-02-15 20:55:12 -0300

    Timeline de eventos implementada no Prologo

---
+++ Assets/FadeScreen/Screen/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LoadScene : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [Tooltip("Nome da cena para carregar ap�s o grab.")]
+    public string sceneToLoad;
+
+    public FadeScreen fadeScreen;
+
+    public void ChangeScene()
+    {
+        StartCoroutine(GoToSceneRoutine());
+    }
+
+    private IEnumerator GoToSceneRoutine()
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        // Launch the new scene
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
